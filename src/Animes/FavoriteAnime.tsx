0f87f77cee21160090f65922877328817c4247d3
kieldoescode30 <kielycarreon123@gmail.com>
2025-04-05 20:59:55 +0800

    feat: right sidebar initial

---
+++ src/Animes/FavoriteAnime.tsx
@@ -0,0 +1,40 @@
+type Props = {
+  title: string;
+  genres: string[];
+  poster: string;
+  rating: number;
+};
+
+const FavoriteAnime = ({ title, genres, poster, rating }: Props) => {
+  return (
+    <div className="flex gap-2">
+      <img
+        src={`${poster}`}
+        alt="solo leveling poster"
+        className="h-[80px] w-[55px] object-cover rounded-lg"
+        width={60}
+        height={120}
+      />
+      <div className="flex flex-col justify-between">
+        <div className="leading-4">
+          <h3 className="text-sm font-semibold">{title}</h3>
+          <span className="text-xs text-gray-400">
+            {genres.map((genre) => genre).join(", ")}
+          </span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <img
+            src="/mal-logo.jpg"
+            width={55}
+            height={20}
+            alt="myanimelist logo"
+            className="h-[20px] object-cover rounded-md"
+          />
+          <span className="text-sm text-gray-500 font-semibold">{rating}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteAnime;
--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,87 +1,51 @@
-import { Search } from "lucide-react";
+import { favoriteAnime, popularAnime } from "../constants/fake-anime-api";
+import PopularAnime from "../Animes/PopularAnime";
+import FavoriteAnime from "../Animes/FavoriteAnime";
 
 const RightSidebar = () => {
   return (
-    <div className="sidebar w-full h-full md:max-w-[300px] max-w-[90px] border-l border-l-gray-400/30 bg-[#F9FAFE] relative font-inter">
-      <div className="md:px-10 px-4 py-4 flex flex-col gap-5">
-        <div className="h-[120px] flex items-center justify-center">
-          <div className="p-2.5 border border-gray-300 rounded-full flex items-center bg-white gap-1.5">
-            <Search className="text-gray-400" />
-            <input
-              type="text"
-              placeholder="Search"
-              className="outline-none focus:outline-none"
-            />
-          </div>
-        </div>
-
+    <div className="sidebar w-full h-full md:max-w-[300px] max-w-[90px] border-l border-l-gray-400/30 bg-[#F9FAFE] relative font-inter overflow-y-auto">
+      <div className="md:px-10 px-4 py-6 flex flex-col justify-center items-center gap-7">
         {/* Popular Animes */}
-        <div className="popular-animes">
-          <h2 className="text-lg font-bold mb-3">Popular Animes</h2>
+        <div className="popular-animes w-full">
+          <h2 className="text-lg font-bold mb-3 text-neutral-800">
+            Popular Animes
+          </h2>
 
           <div className="flex flex-col gap-4">
             {/* Popular anime card */}
-            <div className="flex gap-2">
-              <img
-                src="/solo-leveling-poster.jpg"
-                alt="solo leveling poster"
-                className="h-[95px] object-cover rounded-lg"
-                width={60}
-                height={120}
+            {popularAnime.map((anime) => (
+              <PopularAnime
+                title={anime.title}
+                genres={anime.genres}
+                poster={anime.poster}
+                rating={anime.rating}
               />
-              <div className="flex flex-col justify-between">
-                <div className="leading-5">
-                  <h3 className="text-md font-semibold">Solo Leveling</h3>
-                  <span className="text-xs text-gray-400">
-                    Action, Adventure
-                  </span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <img
-                    src="/mal-logo.jpg"
-                    width={55}
-                    height={20}
-                    alt="myanimelist logo"
-                    className="h-[20px] object-cover rounded-md"
-                  />
-                  <span className="text-sm text-gray-500 font-semibold">
-                    8.6
-                  </span>
-                </div>
-              </div>
-            </div>
+            ))}
 
-            <div className="flex gap-2">
-              <img
-                src="/solo-leveling-poster.jpg"
-                alt="solo leveling poster"
-                className="h-[95px] object-cover rounded-lg"
-                width={60}
-                height={120}
+            <button className="w-full bg-primary text-white py-2.5 rounded-lg hover:bg-primary/80 transition-all duration-200 cursor-pointer">
+              See more
+            </button>
+          </div>
+        </div>
+
+        {/* Favorite Animes */}
+        <div className="favorite-animes w-full">
+          <h2 className="text-lg font-bold mb-3 text-neutral-800">Favorites</h2>
+
+          <div className="flex flex-col gap-4">
+            {favoriteAnime.map((anime) => (
+              <FavoriteAnime
+                title={anime.title}
+                genres={anime.genres}
+                poster={anime.poster}
+                rating={anime.rating}
               />
-              <div className="flex flex-col justify-between">
-                <div className="leading-5">
-                  <h3 className="text-md font-semibold">Solo Leveling</h3>
-                  <span className="text-xs text-gray-400">
-                    Action, Adventure
-                  </span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <img
-                    src="/mal-logo.jpg"
-                    width={55}
-                    height={20}
-                    alt="myanimelist logo"
-                    className="h-[20px] object-cover rounded-md"
-                  />
-                  <span className="text-sm text-gray-500 font-semibold">
-                    8.6
-                  </span>
-                </div>
-              </div>
-            </div>
+            ))}
 
-            <button>See more</button>
+            <button className="w-full bg-primary text-white py-2.5 rounded-lg hover:bg-primary/80 transition-all duration-200 cursor-pointer">
+              See more
+            </button>
           </div>
         </div>
       </div>
